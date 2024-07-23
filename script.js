6ac020d300aad20a72cd367d0ac2a49b7827e7e5
MasomRezaie <masom.rezaie135@gmail.com>
2024-07-22 22:22:41 -0700

    solved linters error

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
 const checkPalindrome = (input) => {
-    const originalInput = input;
+  const originalInput = input;
 
-    if (input === '') {
-        alert('Please input a value');
-        return;
-    }
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
 
-    resultDiv.replaceChildren();
+  resultDiv.replaceChildren();
 
-    const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase(); // remove non alphanumeric characters
-    let resultMessage = `<strong>${originalInput}</strong> ${
-        lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
-    } a palindrome`;
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase(); // remove non alphanumeric characters
+  let resultMessage = `<strong>${originalInput}</strong> ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome`;
 
-    const pTag = document.createElement('p');
-    pTag.className = 'user-input';
-    pTag.innerHTML = resultMessage;
-    resultDiv.appendChild(pTag);
+  const pTag = document.createElement('p');
+  pTag.className = 'user-input';
+  pTag.innerHTML = resultMessage;
+  resultDiv.appendChild(pTag);
 
-    resultDiv.classList.remove('hidden'); 
-}
+  resultDiv.classList.remove('hidden'); 
+};
 
 userInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        checkPalindrome(userInput.value);
-        userInput.value = '';
-    }
-})
-
-checkPalindromeBtn.addEventListener('click', () => {
+  if (e.key === 'Enter') {
     checkPalindrome(userInput.value);
     userInput.value = '';
+  }
+});
+
+checkPalindromeBtn.addEventListener('click', () => {
+  checkPalindrome(userInput.value);
+  userInput.value = '';
 });
